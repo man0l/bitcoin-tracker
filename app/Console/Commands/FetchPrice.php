4e546541aa96a3b4facc47327c1f0cedcfabf14e
Manol Trendafilov <manol.trendafilov@gmail.com>
2023-02-17 19:39:29 +0200

    save price from Bitfinex

--- app/Console/Commands/FetchPrice.php
+++ app/Console/Commands/FetchPrice.php
@@ -2,6 +2,7 @@
 
 namespace App\Console\Commands;
 
+use App\Models\Price;
 use Illuminate\Console\Command;
 use Illuminate\Support\Facades\Http;
 
@@ -27,7 +28,17 @@ class FetchPrice extends Command
     public function handle(): void
     {
         $pair = Http::get(env('FETCH_PRICE_BTC_USD_API_URL'));
-        $price = json_decode($pair->body(), true);
-        var_dump($price);
+        $result = json_decode($pair->body(), true);
+
+        $price = new Price();
+        $price->mid = floatval($result['mid']);
+        $price->bid = floatval($result['bid']);
+        $price->ask = floatval($result['ask']);
+        $price->last_price = floatval($result['last_price']);
+        $price->low = floatval($result['low']);
+        $price->high = floatval($result['high']);
+        $price->volume = floatval($result['volume']);
+        $price->fetched_at = new \DateTime(strtotime($result['timestamp']));
+        $price->save();
     }
 }
